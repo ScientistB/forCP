fc4eb3b0c9801b4e1f408d42b237b799ea4ef0b0
MizokawaShota <mizokawa.shota@donuts.ne.jp>
2019-08-25 14:06:03 +0900

    feat: add template

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <stack>
+#include <string>
+#include <climits>
+#include <algorithm>
+#include <functional>
+#include <string.h>
+#include <numeric>
+#include <cmath>
+
+#define LOOP(N) for(int i=0; i<(N); ++i)
+#define REP(i, N) for(int i=0; i<(N); ++i)
+#define FOR(i, start, end) for(int i=(start); i<(end); ++i)
+#define ALL(a) (a).begin(),(a).end()
+
+using namespace std;
+
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+
+void sayYes() {puts("Yes");}
+void sayNo() {puts("No");}
+
+int _main();
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    _main();
+}
+
+
+int _main() {
+    cout << "This is template." << endl;
+}
