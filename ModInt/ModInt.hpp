05637ce7c5b60323fb146258296d997fe73893aa
MizokawaShota <mizokawa.shota@donuts.ne.jp>
2019-08-26 14:26:12 +0900

    fix: valueがLLONG_MAX-MOD以上だった場合に正しく計算できないバグを修正
    #2

--- ModInt/ModInt.hpp
+++ ModInt/ModInt.hpp
@@ -1,40 +1,44 @@
 
 #ifndef MOD
-#define MOD 1000000007
+static const long long MOD = 1000000007;
 #endif
 
 class ModInt {
     long long v;
 
 public:
-    ModInt(long long value) : v(value%MOD) {}
+    ModInt(long long value) : v(value % MOD) {}
     ModInt() {}
 
     ModInt& operator+=(long long value) {
+        value %= MOD;
         v = (v + value) % MOD;
         return *this;
     }
-    ModInt operator+(long long value) {
+    ModInt operator+(const long long value) const {
         ModInt new_v(v);
         return new_v += value;
     }
     ModInt& operator-=(long long value) {
+        value %= MOD;
         v = (v - value) % MOD;
         return *this;
     }
-    ModInt operator-(long long value) {
+    ModInt operator-(const long long value) const {
         ModInt new_v(v);
         return new_v -= value;
     }
     ModInt& operator*=(long long value) {
+        value %= MOD;
         v = v * value % MOD;
         return *this;
     }
-    ModInt operator*(long long value) {
+    ModInt operator*(const long long value) const {
         ModInt new_v(v);
         return new_v *= value;
     }
-    operator long long() {
+
+    operator long long() const {
         return v;
     }
 };
